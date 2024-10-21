96b75e4bcd443d3325e4402cf1b84ac1e51c6a4d
Ainaya <luthfia.jayyida94@gmail.com>
2024-10-21 11:50:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./pages/signIn";
 import SignUpPage from "./pages/signUp";
 import FormForgotPass from "./pages/newPass"; 
+import ErrorRoute from "./pages/errorRoute";
 
 const App = () => {
   const myRouter = createBrowserRouter([
-     {
+    {
       path: "/",
       element: <div>Halaman Utama</div>,
+      errorElement: <ErrorRoute/>
     },
     {
       path: "/login",
