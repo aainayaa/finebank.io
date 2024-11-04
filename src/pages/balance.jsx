17029967c251469e96d718f7705c1d0f9b07ce5a
Luthfia Jayyida Ainaya F <luthfia.jayyida94@gmail.com>
2024-11-04 17:04:14 +0700

    uts

--- src/pages/balance.jsx
+++ src/pages/balance.jsx
@@ -2,36 +2,27 @@ import Card from "../components/Elements/Card";
 import MainLayout from "../components/Layouts/MainLayout";
 
 const BalancePage = () => {
-    return (
-        <MainLayout type="balance">
-            {/* top content start*/}
-            <div className="mb-8 sm:flex sm:gap-6">
-                <div className="sm:w-1/3">
-                    <Card />
-                </div>
-                <div className="sm:w-1/3">
-                    <Card />
-                </div>
-                <div className="sm:w-1/3">
-                    <Card />
-                </div>
-            </div>
-            {/* top content end*/}
-            {/* bottom content start*/}
-            <div className="mb-8 sm:flex sm:gap-6">
-                <div className="sm:w-1/3">
-                    <Card />
-                </div>
-                <div className="sm:w-1/3">
-                    <Card />
-                </div>
-                <div className="sm:w-1/3">
-                    <Card />
-                </div>
-            </div>
-            {/* bottom content end*/}
-        </MainLayout>
-    );
+  return (
+    <MainLayout type="balance">
+      {/* top content start*/}
+      <div className="md:grid md:grid-cols-3 md:gap-x-6">
+        <Card title="Balances" />
+        <Card title="&nbsp;" />
+        <Card 
+        title="&nbsp;" 
+        desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        /> 
+        </div>
+
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+        <Card desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
+        <Card />
+        <Card />
+        </div>
+      {/* bottom content end*/}
+    </MainLayout>
+  );
 };
 
 export default BalancePage;
